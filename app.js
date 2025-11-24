af2ef68d64abcd2cdc987ec667891d2de51115ef
Connor <connorjohnhughes@gmail.com>
2025-11-23 19:45:02 -0800

    Added my sql database through digital Ocean

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 import express from "express";
+import mysql2 from "mysql2";
+import dotenv from 'dotenv';
 
+
+dotenv.config();
 const app = express();
+
+const pool = mysql2.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+}).promise()
+
+
+
 const PORT = 3003;
 const contacts = [];
 
@@ -10,6 +25,15 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 
+
+app.get('/db-test', async(req,res) =>{
+    try{
+        const [contacts] = await pool.query('SELECT * FROM contacts');
+        res.send(contacts);
+    } catch(err){
+        console.error('Database error:', err)
+    }
+});
 
 
 
@@ -22,23 +46,53 @@ app.get('/contact', (req,res) => {
 app.get('/confirm', (req,res) => {
     res.render('confirmation');
 });
-app.get('/admin', (req,res) => {
 
-    res.render('admin', {contacts});
+
+app.get('/admin', async (req, res) => {
+    try{
+        const [contacts] = await pool.query('SELECT * FROM contacts ORDER BY timestamp DESC');
+        res.render('admin', {contacts});
+    } catch(err){
+        console.error('Database error:', err);
+        res.status(500).send('Error loading contacts' + err.message);
+    }
 });
 
 app.listen(PORT,() =>{
     console.log(`Server is running at http://localhost:${PORT}`);
 });  
 
-app.post('/submit-form', (req,res) => {
-    const contact = req.body;
-    
-    // console.log(contact);
+app.post('/submit-form', async (req,res) => {
+    try{
+        const contact = req.body;
+        console.log('New contact submitted: ', contact);
 
-    contact.timestamp = new Date().toLocaleDateString();
+        const sql = 
+       `INSERT INTO contacts (firstName, lastName, jobTitle, company, linkedIn, email, meetWhere, other, message, mailingFormat, mailingList)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)`;
 
-    contacts.push(contact);
+            const params = [
+            contact.fName,
+            contact.lName,
+            contact.job,
+            contact.company,
+            contact.linkedin || '',
+            contact.email,
+            contact.meet,
+            contact.other || '',
+            contact.message || '',
+            contact.format || '',
+            contact.mailing || ''
+            ];
 
-    res.render('confirmation', {contact});
+
+        const[result] = await pool.execute(sql, params);
+        console.log('contact saved with Id:', result.insertId)
+         res.render('confirmation', {contact});
+    }catch(err){
+        console.error('Database error:', err);
+        res.status(500).send('Sorry there is a problem processing your Information:  ' + err.message);
+    }
+
+   
 });
